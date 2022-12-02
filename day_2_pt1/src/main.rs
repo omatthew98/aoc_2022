a593f00f47762f8bda2d100549059cf31c68871f
Matthew Owen <omatthew98@berkeley.edu>
2022-12-02 13:51:38 -0800

    adding in simpler non enum solution

---
+++ day_2_pt1/src/main.rs
@@ -0,0 +1,57 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+const ROCK: &str = "A";
+const PAPER: &str = "B";
+const SCISSORS: &str = "C";
+
+fn convert(s: &str) -> &str {
+    match s {
+        "X" => ROCK,
+        "Y" => PAPER,
+        "Z" => SCISSORS,
+        _   => ROCK
+    }
+}
+
+fn value_of_match(opp_mv: &str, my_mov: &str) -> i32 {
+    let outcome_val = match (opp_mv, convert(my_mov)) {
+        // ties
+        (ROCK, ROCK) | (PAPER, PAPER) | (SCISSORS, SCISSORS) => 3,
+        // wins
+        (ROCK, PAPER) | (PAPER, SCISSORS) | (SCISSORS, ROCK) => 6,
+        _ => 0
+    };
+    let play_val = match convert(my_mov) {
+        ROCK => 1,
+        PAPER => 2,
+        SCISSORS => 3,
+        _ => 0
+    };
+    return outcome_val + play_val;
+}
+
+fn value_of_line(line: String) -> i32 {
+    let vec: Vec<&str> = line.split(" ").collect();
+
+    return value_of_match(vec.get(0).unwrap_or_else(|| &""), vec.get(1).unwrap_or_else(|| &""));
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut score = 0;
+
+    for line in reader.lines() {
+        score += value_of_line(line.unwrap_or_default());
+    }
+
+    println!("{:?}", score);
+
+    Ok(())
+}
